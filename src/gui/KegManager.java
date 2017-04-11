35524025f3ec9d441d19480fc9a4d5d13ebc5dea
Aidan Wilson <aidan.sw0@gmail.com>
2017-04-11 16:49:52 -0700

    prototyping buttons

--- src/gui/KegManager.java
+++ src/gui/KegManager.java
@@ -1,9 +1,18 @@
 package gui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Timer;
+import java.util.TimerTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import dweetHandler.ContentType;
-import dweetHandler.DweetParser;
 import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
 
@@ -58,10 +67,8 @@ public class KegManager {
     public void updateContent(ContentType type, double val) {
         if (type == ContentType.WEIGHT) {
             weight.set(convertWeightToHeight(val));
-            System.out.println(weight.get());
         } else if (type == ContentType.TEMP) {
             temp.set(convertTempToHeight(val));
-            System.out.println(temp.get());
         }
     }
 
@@ -110,3 +117,69 @@ public class KegManager {
     }
 
 }
+
+
+
+
+
+/**
+ * This class is responsible for reading and parsing the JSON data polled from
+ * Dweet.io.
+ * 
+ * @author Aidan
+ *
+ */
+class DweetParser extends TimerTask {
+
+    private final KegManager kegManager;
+    private final String urlString;
+
+    public DweetParser(KegManager kegManager, String url) {
+        this.kegManager = kegManager;
+        this.urlString = url;
+    }
+
+    /**
+     * Makes an HTTP request to the given Dweet url then reads and parses the
+     * JSON data. Updated values are then sent to the KegManager via
+     * sendToManager().
+     */
+    @Override
+    public void run() {
+        BufferedReader reader;
+        String inputLine;
+
+        try {
+            // Read JSON string form URL
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            inputLine = reader.readLine();
+
+            JSONObject jsonDweet = (JSONObject) new JSONTokener(inputLine).nextValue();
+            JSONArray with = jsonDweet.getJSONArray("with");
+            JSONObject withObject = with.getJSONObject(0); // with only contains
+                                                           // one element
+            JSONObject content = withObject.getJSONObject("content");
+
+            sendToManager(ContentType.WEIGHT, content.getDouble("weight"));
+            sendToManager(ContentType.TEMP, content.getDouble("temp"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Sends newly polled values from the Dweet url to the KegManager.
+     * 
+     * @param type, ContentType which gives the type of value to be sent.
+     * @param val, double value polled from the Dweet url.
+     */
+    private void sendToManager(ContentType type, double val) {
+        kegManager.updateContent(type, val);
+    }
+
+}
+
---
+++ src/gui/TempClass.java
@@ -0,0 +1,141 @@
+package gui;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class TempClass extends Application {
+    
+    int currentBeer = 0;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Stage window;
+
+        window = primaryStage;
+        window.setTitle("Beer Keg Monitor");
+        
+        
+
+        Map<String, Integer> beerTypeLikes = new HashMap<String, Integer>();
+        beerTypeLikes.put("beer0", 0);
+        beerTypeLikes.put("beer1", 0);
+        beerTypeLikes.put("beer2", 0);
+        beerTypeLikes.put("beer3", 0);
+        
+        List<String> beerTypes = new ArrayList<String>();
+        beerTypes.add("beer0");
+        beerTypes.add("beer1");
+        beerTypes.add("beer2");
+        beerTypes.add("beer3");
+        
+        Button like = new Button("Like");
+        like.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                //event.consume();
+                String beerToUpvote = beerTypes.get(currentBeer);
+                beerTypeLikes.put(beerToUpvote, beerTypeLikes.get(beerToUpvote) + 1);
+            }
+        });
+
+        Button left = new Button("Left");
+        left.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (currentBeer > 0) {
+                    currentBeer--;
+                } else if (currentBeer == 0) {
+                    currentBeer = beerTypes.size() - 1;
+                }
+            }
+        });
+
+        Button right = new Button("Right");
+        right.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if (currentBeer < beerTypes.size() - 1) {
+                    currentBeer++;
+                } else if (currentBeer == beerTypes.size() - 1) {
+                    currentBeer = 0;
+                }
+            }
+        });
+
+        TextField enterNewBeer = new TextField();
+        Button enter = new Button("Enter");
+        enter.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                String beerToAdd = enterNewBeer.getText();
+                enterNewBeer.clear();
+                
+                if (!beerTypeLikes.containsKey(beerToAdd)) {
+                    beerTypeLikes.put(beerToAdd, 0);
+                } else {
+                    System.out.println("This beer is already in the database!");
+                }
+            }
+        });
+
+        BorderPane leftPane = new BorderPane();
+        BorderPane.setAlignment(left, Pos.TOP_LEFT);
+        leftPane.setCenter(left);
+        leftPane.setVisible(true);
+        
+        BorderPane rightPane = new BorderPane();
+        BorderPane.setAlignment(right, Pos.TOP_RIGHT);
+        rightPane.setCenter(right);
+        rightPane.setVisible(true);
+        
+        BorderPane likePane = new BorderPane();
+        BorderPane.setAlignment(like, Pos.BOTTOM_LEFT);
+        likePane.setCenter(like);
+        likePane.setVisible(true);
+        
+        BorderPane textPane = new BorderPane();
+        BorderPane.setAlignment(enterNewBeer, Pos.CENTER);
+        textPane.setCenter(enterNewBeer);
+        textPane.setVisible(true);
+        
+        BorderPane enterPane = new BorderPane();
+        BorderPane.setAlignment(enter, Pos.BASELINE_RIGHT);
+        enterPane.setCenter(enter);
+        enterPane.setVisible(true);
+        
+        FlowPane root = new FlowPane();
+        root.getChildren().add(leftPane);
+        root.getChildren().add(rightPane);
+        root.getChildren().add(likePane);
+        root.getChildren().add(textPane);
+        root.getChildren().add(enterPane);
+        root.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(root, 400, 300);
+        window.setScene(scene);
+        window.show();
+    }
+}
