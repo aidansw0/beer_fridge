12bacbae13c0624fdb2e513f76d074493ad7286c
Richard <richardchen.93@gmail.com>
2017-04-11 17:14:26 -0700

    Added all frames, window resizable

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -40,7 +39,7 @@ public class Main extends Application {
         Image img2 = new Image("img/kegheader.png");
         ImageView kegheader = new ImageView();
         kegheader.setImage(img2);
-        kegheader.setFitWidth(445);
+        kegheader.setFitHeight(53);
         kegheader.setPreserveRatio(true);
 
         Polygon kegMeter = new Polygon();
@@ -70,15 +69,75 @@ public class Main extends Application {
         kegFrame.setLeft(keg);
         kegFrame.setCenter(kegMeterStack);
 
-        BorderPane.setAlignment(keg, Pos.CENTER_LEFT);
-        BorderPane.setMargin(keg, new Insets(0,5,0,30));
+        BorderPane.setAlignment(keg, Pos.BOTTOM_LEFT);
+        BorderPane.setMargin(keg, new Insets(20,5,30,30));
 
-        // FlowPane
-        FlowPane root = new FlowPane();
-        root.getChildren().add(kegFrame);
-        root.setAlignment(Pos.CENTER);
+        // Temperature Frame
+        Image img3 = new Image("img/tempheader.png");
+        ImageView tempheader = new ImageView();
+        tempheader.setImage(img3);
+        tempheader.setFitHeight(53);
+        tempheader.setPreserveRatio(true);
 
-        Scene scene = new Scene(root, 445, 450);
+        BorderPane tempFrame = new BorderPane();
+        tempFrame.setPrefSize(715,270);
+        tempFrame.setMaxWidth(715);
+        tempFrame.setStyle("-fx-background-color: #2d2d2d");
+        tempFrame.setTop(tempheader);
+
+        // Voting Frame
+        Image img4 = new Image("img/votingheader.png");
+        ImageView votingheader = new ImageView();
+        votingheader.setImage(img4);
+        votingheader.setFitHeight(53);
+        votingheader.setPreserveRatio(true);
+
+        BorderPane votingFrame = new BorderPane();
+        votingFrame.setPrefSize(715,150);
+        votingFrame.setMaxWidth(715);
+        votingFrame.setStyle("-fx-background-color: #2d2d2d");
+        votingFrame.setTop(votingheader);
+
+        // Combine Temperature and Voting Frame
+        BorderPane rightPane = new BorderPane();
+        rightPane.setPrefSize(715,450);
+        rightPane.setTop(tempFrame);
+        rightPane.setCenter(votingFrame);
+
+        BorderPane.setAlignment(tempFrame, Pos.TOP_LEFT);
+        BorderPane.setMargin(tempFrame, new Insets(0,0,15,0));
+        BorderPane.setAlignment(votingFrame, Pos.TOP_LEFT);
+        BorderPane.setMargin(votingFrame, new Insets(15,0,0,0));
+
+        // Footer Frame
+        Image img5 = new Image("img/footerheader.png");
+        ImageView footerheader = new ImageView();
+        footerheader.setImage(img5);
+        footerheader.setFitHeight(53);
+        footerheader.setPreserveRatio(true);
+
+        BorderPane footerFrame = new BorderPane();
+        footerFrame.setPrefSize(1190,220);
+        footerFrame.setMaxWidth(1190);
+        footerFrame.setStyle("-fx-background-color: #2d2d2d");
+        footerFrame.setTop(footerheader);
+
+        // Full Scene
+        BorderPane root = new BorderPane();
+        root.setPrefSize(1190,450);
+        root.setStyle("-fx-background-color: #1e1e1e");
+        root.setLeft(kegFrame);
+        root.setCenter(rightPane);
+        root.setBottom(footerFrame);
+
+        BorderPane.setAlignment(kegFrame, Pos.CENTER_RIGHT);
+        BorderPane.setMargin(kegFrame, new Insets(45,15,15,50));
+        BorderPane.setAlignment(rightPane, Pos.CENTER_LEFT);
+        BorderPane.setMargin(rightPane, new Insets(45,50,15,15));
+        BorderPane.setAlignment(footerFrame, Pos.TOP_LEFT);
+        BorderPane.setMargin(footerFrame, new Insets(15,50,45,50));
+
+        Scene scene = new Scene(root, 1280, 800);
         window.setScene(scene);
         window.show();
     }
